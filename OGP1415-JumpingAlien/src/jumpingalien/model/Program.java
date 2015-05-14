61ce97c9fbd667971748112b7966e080948cd141
mithom <mi_thom@hotmail.com>
2015-05-14 17:53:34 +0200

    implementation of hierarchy from statements

--- OGP1415-JumpingAlien/src/jumpingalien/model/Program.java
+++ OGP1415-JumpingAlien/src/jumpingalien/model/Program.java
@@ -4,12 +4,40 @@ import java.util.HashMap;
 import java.util.Map;
 
 import jumpingalien.model.gameObject.GameObject;
+import jumpingalien.program.internal.Statement;
 import jumpingalien.program.internal.Type;
 
 public class Program {
 	
+	private GameObject gameObject;
+	private Statement statement;
+	
 	public Program(){
 		
+	}
+	
+	public void setGameObject(GameObject gameObject){//TODO add checkers
+		this.gameObject = gameObject;
+	}
+	
+	public GameObject getGameObject(){
+		return gameObject;
+	}
+	
+	private double executeNext(double dt){
+		return statement.executeNext(dt);
+	}
+	
+	public void executeTime(double dt){
+		while(dt>0){
+			dt = executeNext(dt);
+		}
+		//TODO: implement this function
+	}
+	
+	public void addStatement(Statement statement){//TODO add checkers
+		this.statement = statement;
+		statement.addProgram(this);
 	}
 	
 	public enum Direction {
@@ -41,5 +69,9 @@ public class Program {
 		for(String key:globals.keySet()){
 			addGlobal(key, globals.get(key));
 		}
+	}
+	
+	public void resetGlobals(){
+		
 	}
 }
--- OGP1415-JumpingAlien/src/jumpingalien/program/internal/Statement.java
+++ OGP1415-JumpingAlien/src/jumpingalien/program/internal/Statement.java
@@ -1,5 +1,57 @@
 package jumpingalien.program.internal;
 
-public class Statement {
+import jumpingalien.model.Program;
 
+public class Statement {
+	private Program program;
+	private boolean done;
+	private Statement nextStatement;
+	private Statement previousStatement;
+	
+	public void addProgram(Program program){
+		this.program = program;
+	}
+	
+	public Program getProgram(){
+		done = false;
+		return program;
+	}
+	
+	public Statement(){
+	}
+	
+	public boolean isDone(){
+		return done;
+	}
+	
+	public double executeNext(double dt){
+		if(dt<=0)
+			return 0;
+		if(!isDone()){
+			execute();
+			dt-= 0.001;
+		}
+		if(nextStatement != null){
+			nextStatement.addPreviousStatement(this);
+			return nextStatement.executeNext(dt);
+		}
+		reset();
+		return dt;
+	}
+	
+	private void addPreviousStatement(Statement statement){
+		previousStatement = statement;
+	}
+	
+	private void execute(){
+		//TODO: implement this function
+	}
+	
+	private void reset(){
+		done = false;
+		if(previousStatement != null)
+			previousStatement.reset();
+		else
+			program.resetGlobals();
+	}
 }
